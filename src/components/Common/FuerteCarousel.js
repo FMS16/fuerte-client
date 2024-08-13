c0d197f83aa4af838ee9fde075037eee924a8fca
FMS16 <101989417+FMS16@users.noreply.github.com>
2024-08-13 19:25:12 -0300

    change-slider-v2

--- src/components/Common/FuerteCarousel.js
+++ src/components/Common/FuerteCarousel.js
@@ -1,21 +1,50 @@
 import logo from "../../assets/images/logoRecortadoFino.png";
 import Image from "next/image";
 const FuerteCarousel = () => {
-    const slides = [];
-    for (let i = 0; i < 14; i++) {
-        slides.push(
-            <div className="slide" key={i}>
-                <Image src={logo} alt="Logo" />
-            </div>
-        );
-    }
+
     return (
-        <div className="slider">
-            <div className="slide-track">
-                {slides}
-                {slides}
+        <div class="slider">
+        <div class="slide-track">
+            <div class="slide">
+                <Image src={logo} width={125} height={25} alt="Logo" />
+            </div>
+            <div class="slide">
+                <Image src={logo} width={125} height={25} alt="Logo" />
+            </div>
+            <div class="slide">
+                <Image src={logo} width={125} height={25} alt="Logo" />
+            </div>
+            <div class="slide">
+                <Image src={logo} width={125} height={25} alt="Logo" />
+            </div>
+            <div class="slide">
+                <Image src={logo} width={125} height={25} alt="Logo" />
+            </div>
+            <div class="slide">
+                <Image src={logo} width={125} height={25} alt="Logo" />
+            </div>
+            <div class="slide">
+                <Image src={logo} width={125} height={25} alt="Logo" />
+            </div>
+
+            <div class="slide">
+                <Image src={logo} width={125} height={25} alt="Logo" />
+            </div>
+            <div class="slide">
+                <Image src={logo} width={125} height={25} alt="Logo" />
+            </div>
+            <div class="slide">
+                <Image src={logo} width={125} height={25} alt="Logo" />
+            </div>
+            <div class="slide">
+                <Image src={logo} width={125} height={25} alt="Logo" />
+            </div>
+            <div class="slide">
+                <Image src={logo} width={125} height={25} alt="Logo" />
             </div>
         </div>
+    </div>
+
     );
 };
 
