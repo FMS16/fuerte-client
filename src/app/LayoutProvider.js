874bbcb2a6a2eae598c94872b06dca34d197e42b
FMS16 <101989417+FMS16@users.noreply.github.com>
2024-09-21 16:02:23 -0300

    Commit-changes-optimizing-v8

--- src/app/LayoutProvider.js
+++ src/app/LayoutProvider.js
@@ -1,21 +1,28 @@
-'use client';
+"use client";
 
-import Footer from '@/components/Common/Footer';
-import Header from '@/components/Common/Header';
-import { usePathname } from 'next/navigation';
-import { ToastContainer } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
-import "./globals.css";
+import dynamic from 'next/dynamic';
+import { useEffect } from 'react';
+
+const ToastContainer = dynamic(
+  () => import('react-toastify').then((mod) => mod.ToastContainer),
+  { ssr: false }
+);
 
 export const LayoutProvider = ({ children }) => {
-    const pathname = usePathname();
-    
-    return (
-        <>
-            {pathname !== "/checkout" && pathname !== "/login" && <Header />}
-            {children}
-            {pathname !== "/checkout" && pathname !== "/login" && <Footer />}
-            <ToastContainer />
-        </>
-    );
+  const pathname = usePathname();
+  
+  useEffect(() => {
+    if (pathname !== '/checkout' && pathname !== '/login') {
+      import('react-toastify/dist/ReactToastify.css');
+    }
+  }, [pathname]);
+
+  return (
+    <>
+      {pathname !== "/checkout" && pathname !== "/login" && <Header />}
+      {children}
+      {pathname !== "/checkout" && pathname !== "/login" && <Footer />}
+      <ToastContainer />
+    </>
+  );
 };
