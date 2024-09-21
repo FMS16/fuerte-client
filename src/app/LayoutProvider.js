8e4faec05f0633d8c8e739e744e71d87f25e212c
FMS16 <101989417+FMS16@users.noreply.github.com>
2024-09-21 14:13:01 -0300

    Commit-changes-optimizing-v6

--- src/app/LayoutProvider.js
+++ src/app/LayoutProvider.js
@@ -4,6 +4,7 @@ import Footer from '@/components/Common/Footer';
 import Header from '@/components/Common/Header';
 import { usePathname } from 'next/navigation';
 import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
 
 export const LayoutProvider = ({ children }) => {
