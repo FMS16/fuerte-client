c2b259c3948212ba93c6f4516c7603205f0a311c
FMS16 <101989417+FMS16@users.noreply.github.com>
2024-09-25 11:03:25 -0300

    Commit-changes-analytics

--- src/components/GoogleAnalyticsTracking.js
+++ src/components/GoogleAnalyticsTracking.js
@@ -7,11 +7,11 @@ export const GoogleAnalyticsTracking = () => {
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-GOOGLEID" />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-          window.dataLayer = window.dataLayer || [];
+   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-S8N7STLR9S');
+  gtag('config', 'G-6FWC6ZKWJJ');
         `}
       </Script>
     </>
