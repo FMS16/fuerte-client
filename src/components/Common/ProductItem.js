1dc75168a52167dee9fd7af1bcd8d7ac801eb5a9
Fabricio <martinsecondo16@gmail.com>
2024-11-04 11:19:37 -0300

    Cambios precio con descuento

--- src/components/Common/ProductItem.js
+++ src/components/Common/ProductItem.js
@@ -95,7 +95,6 @@ const ProductItem = ({ product, isWishlistComponent }) => {
                     </motion.button>}
                 <div className='product-item-details'>
                     <h1 dangerouslySetInnerHTML={{ __html: product.name }} />
-                    <h2 className='price-before'>${currency === 'USD' ? (product.priceUSD * 1.10).toFixed(0) : ((product.priceUYU * 100) / 90).toFixed(0)}</h2>
                     <h2 className='price-now'>${currency === 'USD' ? product.priceUSD : product.priceUYU}</h2>
                 </div>
             </>
@@ -124,7 +123,6 @@ const ProductItem = ({ product, isWishlistComponent }) => {
                     </motion.button>}
                 <div className='product-item-details'>
                     <h1 dangerouslySetInnerHTML={{ __html: product.name }} />
-                    <h2 className='price-before'>${currency === 'USD' ? ((product.priceUSD * 100) / 90).toFixed(0) : ((product.priceUYU * 100) / 90).toFixed(0)}</h2>
                     <h2 className='price-now'>${currency === 'USD' ? product.priceUSD : product.priceUYU}</h2>
                 </div>
             </>)
