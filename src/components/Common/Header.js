005f3f2c0c430c42a070ce9850dab39b9cad355f
Fabricio <martinsecondo16@gmail.com>
2025-03-20 10:03:00 -0300

    cambios header y precios en negro

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -64,22 +64,22 @@ const Header = () => {
                     <div className="ticker">
                         <div className="ticker-inner">
                             <div className="ticker-item">
-                                <p className='size-small'>Hola Preciosa! Te regalamos 10% de descuento en TODA la web para darte la motivaci&oacute;n que necesitabas! 🎉</p>
+                                <p className='size-small'>Hola Preciosa!</p>
                             </div>
                             <div className="ticker-item">
-                            <p className='size-small'>Hola Preciosa! Te regalamos 10% de descuento en TODA la web para darte la motivaci&oacute;n que necesitabas! 🎉</p>
+                            <p className='size-small'>Hola Preciosa!</p>
                             </div>
                             <div className="ticker-item">
-                            <p className='size-small'>Hola Preciosa! Te regalamos 10% de descuento en TODA la web para darte la motivaci&oacute;n que necesitabas! 🎉</p>
+                            <p className='size-small'>Hola Preciosa!</p>
                             </div>
                             <div className="ticker-item">
-                            <p className='size-small'>Hola Preciosa! Te regalamos 10% de descuento en TODA la web para darte la motivaci&oacute;n que necesitabas! 🎉</p>
+                            <p className='size-small'>Hola Preciosa!</p>
                             </div>
                             <div className="ticker-item">
-                            <p className='size-small'>Hola Preciosa! Te regalamos 10% de descuento en TODA la web para darte la motivaci&oacute;n que necesitabas! 🎉</p>
+                            <p className='size-small'>Hola Preciosa!</p>
                             </div>
                             <div className="ticker-item">
-                            <p className='size-small'>Hola Preciosa! Te regalamos 10% de descuento en TODA la web para darte la motivaci&oacute;n que necesitabas! 🎉</p>
+                            <p className='size-small'>Hola Preciosa!</p>
                             </div>
                         </div>
                     </div>
